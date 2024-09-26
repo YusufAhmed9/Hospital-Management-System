e73af412d09f4237156e306038a4111cd6ab1553
YousefAdel777 <www.yoyoadel@gmail.com>
2024-09-26 21:22:27 +0300

    add: Hospital CRUD logic, Admin menu

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,8 +1,135 @@
 package org.example;
 
+import java.util.Scanner;
+import java.sql.*;
+
 public class Admin extends User {
+
     public Admin(User user){
         setUsername(user.getUsername());
         setPassword(user.getPassword());
+    }
+
+    public static void adminMenu (Connection conn) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Admin Dashboard");
+        System.out.println("[ 1 ]: Add Hospital");
+        System.out.println("[ 2 ]: Delete Hospital");
+        System.out.println("[ 3 ]: Edit Hospital Info");
+        System.out.println("[ 4 ]: Display All Hospitals");
+        System.out.println("[ 5 ]: Exit");
+        while (true) {
+            System.out.print("Option: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            if (option <= 4 && option >= 1) {
+                if (option == 1) {
+                    String name;
+                    float reservationPrice;
+                    while (true) {
+                        System.out.print("Hospital Name: ");
+                        name = scanner.nextLine();
+                        if (name.isEmpty()) {
+                            System.out.println("Name Can't Be Empty.");
+                            continue;
+                        }
+                        break;
+                    }
+                    while (true) {
+                        System.out.print("Reservation Price: ");
+                        reservationPrice = scanner.nextFloat();
+                        if (reservationPrice <= 0) {
+                            System.out.println("Price Can't Be Equal To Or Less Than 0.");
+                            continue;
+                        }
+                        break;
+                    }
+                    Hospital hospital = new Hospital(name, reservationPrice);
+                    Hospital.createHospital(conn, hospital);
+                }
+                else if (option == 2) {
+                    if (Hospital.count(conn) > 0) {
+                        Hospital.displayHospitals(conn);
+                        while (true) {
+                            System.out.print("Hospital ID To Delete: ");
+                            int id = scanner.nextInt();
+                            Hospital hospital = Hospital.getHospitalById(conn, id);
+                            if (hospital != null) {
+                                hospital.delete(conn);
+                                break;
+                            }
+                            System.out.println("No Hospital With That ID.");
+                        }
+                    }
+                    else {
+                        System.out.println("No Hospitals To Delete.");
+                    }
+                }
+                else if (option == 3) {
+                    if (Hospital.count(conn) > 0) {
+                        Hospital.displayHospitals(conn);
+                        while (true) {
+                            System.out.print("Hospital ID To Edit: ");
+                            int id = scanner.nextInt();
+                            Hospital hospital = Hospital.getHospitalById(conn, id);
+                            if (hospital != null) {
+                                while (true) {
+                                    int editOption;
+                                    System.out.println("[ 1 ]: Edit Name");
+                                    System.out.println("[ 2 ]: Edit Reservation Price");
+                                    System.out.print("Option: ");
+                                    editOption = scanner.nextInt();
+                                    if (editOption == 1) {
+                                        scanner.nextLine();
+                                        String name = "";
+                                        while (true) {
+                                            System.out.print("New Name: ");
+                                            name = scanner.nextLine();
+                                            if (!name.isEmpty()) {
+                                                hospital.setName(name);
+                                                break;
+                                            }
+                                            System.out.println("Name Can't Be Empty.");
+                                        }
+                                        break;
+                                    }
+                                    else if (editOption == 2) {
+                                        float reservationPrice;
+                                        while (true) {
+                                            System.out.print("New Reservation Price: ");
+                                            reservationPrice = scanner.nextFloat();
+                                            if (reservationPrice > 0) {
+                                                hospital.setReservationPrice(reservationPrice);
+                                                break;
+                                            }
+                                            System.out.println("Reservation Price Can't Be Equal To Or Less Than 0.");
+                                        }
+                                        break;
+                                    }
+                                    else {
+                                        System.out.println("Invalid Option.");
+                                    }
+                                }
+                                hospital.edit(conn, hospital);
+                                break;
+                            }
+                            System.out.println("No Hospital With That ID.");
+                        }
+                    }
+                    else {
+                        System.out.println("No Hospitals To Edit.");
+                    }
+                }
+                else {
+                    Hospital.displayHospitals(conn);
+                }
+            }
+            else if (option == 5) {
+                break;
+            }
+            else {
+                System.out.println("Invalid Option.");
+            }
+        }
     }
 }
--- src/main/java/org/example/Hospital.java
+++ src/main/java/org/example/Hospital.java
@@ -1,4 +1,143 @@
 package org.example;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Hospital {
+    private int id;
+    private String name;
+    private float reservationPrice;
+    private int rating;
+
+    Hospital(String name, float reservationPrice) {
+        setName(name);
+        setRating(0);
+        setReservationPrice(reservationPrice);
+    }
+
+    Hospital(String name, float reservationPrice, int rating) {
+        setName(name);
+        setRating(rating);
+        setReservationPrice(reservationPrice);
+    }
+
+    Hospital(int id, String name, float reservationPrice, int rating) {
+        setId(id);
+        setName(name);
+        setRating(rating);
+        setReservationPrice(reservationPrice);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setReservationPrice(float reservationPrice) {
+        this.reservationPrice = reservationPrice;
+    }
+
+    public float getReservationPrice() {
+        return reservationPrice;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public static Hospital getHospitalById(Connection conn, int id) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM hospitals WHERE id = " + id);
+        if (rs.next()) {
+            return new Hospital(rs.getInt("id"), rs.getString("name"), rs.getFloat("reservationPrice"), rs.getInt("rating"));
+        }
+        return null;
+    }
+
+    public static Hospital getHospitalByName(Connection conn, String name) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM hospitals WHERE name = '" + name + "'");
+        if (rs.next()) {
+            return new Hospital(rs.getInt("id"), rs.getString("name"), rs.getFloat("reservationPrice"), rs.getInt("rating"));
+        }
+        return null;
+    }
+
+    public static void createHospital(Connection conn, Hospital hospital) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO hospitals(name, reservationPrice, rating) values(?, ?, ?)");
+        preparedStatement.setString(1, hospital.getName());
+        preparedStatement.setFloat(2, hospital.getReservationPrice());
+        preparedStatement.setInt(3, hospital.getRating());
+        preparedStatement.executeUpdate();
+    }
+
+    public static void getHospitals(Connection conn, ArrayList<Hospital> hospitals) throws  SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM hospitals");
+        while (rs.next()) {
+            Hospital hospital = new Hospital(rs.getString("name"), rs.getFloat("reservationPrice"), rs.getInt("rating"));
+            hospitals.add(hospital);
+        }
+    }
+
+    public static void displayHospitals(Connection conn) throws SQLException {
+        if (count(conn) > 0) {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM hospitals");
+            System.out.printf("%15s", "ID |");
+            System.out.printf("%15s", "Name |");
+            System.out.printf("%20s", "Reservation Price |");
+            System.out.printf("%15s", "Rating |\n");
+            while (rs.next()) {
+                System.out.printf("%15s", rs.getInt("id") + " |");
+                System.out.printf("%15s", rs.getString("Name") + " |");
+                System.out.printf("%20s", rs.getFloat("reservationPrice") + " |");
+                System.out.printf("%15s", rs.getInt("rating") + " |\n");
+            }
+        }
+        else {
+            System.out.println("No Hospitals To Display.");
+        }
+    }
+
+    public void delete(Connection conn) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement("DELETE FROM hospitals WHERE id = ?");
+        preparedStatement.setInt(1, getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public void edit(Connection conn, Hospital hospital) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement("UPDATE hospitals SET name = ?, reservationPrice = ?, rating = ? WHERE id = ?");
+        preparedStatement.setString(1, hospital.getName());
+        preparedStatement.setFloat(2, hospital.getReservationPrice());
+        preparedStatement.setInt(3, hospital.getRating());
+        preparedStatement.setInt(4, getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public static int count(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT COUNT(*) FROM hospitals");
+        return rs.getInt(1);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
