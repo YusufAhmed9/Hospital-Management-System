823f91f3d42e809d34f7118b49259a58d98a7827
YousefAdel777 <www.yoyoadel@gmail.com>
2024-11-03 19:36:01 +0200

    edit: Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,5 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        User user = new User("1", "test_user", "aa");
-        Patient patient = new Patient("1", "test_user", "aa");
-        try {
-            patient.Interface();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
